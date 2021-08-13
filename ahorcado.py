c0eb2cf25555f455e5a35be057841ec37eeedbf2
Luisda <luiszavala@live.com.mx>
2021-08-13 15:40:21 -0500

    Start coding

--- ahorcado.py
+++ ahorcado.py
@@ -1,12 +1,65 @@
 import random
 PROPS_AHORCADO = ['''
 
-    +---+
-    |   |
+    +---+ |  |
+        |
         |
         |
         |
         |
    =======''','''
 
-]
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+   =======''','''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+   =======''','''
+
+    +---+  
+    |   |
+    O   |
+   /|   |
+        |
+        |
+   =======''','''
+
+    +---+  
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+   =======''','''
+
+    +---+  
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+   =======''','''
+
+    +---+  
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+   =======''']
+words = 'muerte destrucción mayhem snow nocturnal ulver'.split()
+
+def get_randword(word_list):
+    return word_list[random.randint(0, len(word_list)-1)]
+def show_game(PROPS_AHORCADO, wrong_letters, correct_letters, secret_word):
+    print(PROPS_AHORCADO[len(wrong_letters)])
+    print()
